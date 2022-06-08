9d70347ede42326108ac7614de37cf933f4727a3
zhenghy <1014660095@qq.com>
2022-06-08 21:45:08 +0800

    build(login/index): 测试提交规范
    测试提交规范
    
    测试提交规范

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,9 +3,7 @@
     <a-card class="login" :bordered="false" :body-style="{ width: '480px', padding: '30px 0' }">
       <div class="login_right_container">
         <div class="title_box">
-          <h2 class="title">
-
-            账密 登 录</h2>
+          <h2 class="title"> 账密登录</h2>
         </div>
         <!-- form -->
         <login-form />
@@ -14,37 +12,37 @@
   </div>
 </template>
 <script setup lang="ts">
-import LoginForm from './Form.vue';
+  import LoginForm from './Form.vue';
   // import { APP_TITLE } from '../../../config/constant';
 </script>
 <style lang="less" scoped>
-.login-container {
-  width: 100vw;
-  height: 100vh;
-  background-image: linear-gradient(to right, #74ebd5 0%, #9face6 100%);
+  .login-container {
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(to right, #74ebd5 0%, #9face6 100%);
 
-  .login {
-    border-radius: 10px;
-    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.4);
+    .login {
+      border-radius: 10px;
+      box-shadow: 0 3px 10px rgba(0, 0, 0, 0.4);
 
-    // background-image: linear-gradient(to top, #9795f0 0%, #fbc8d4 100%);
-    &_right {
-      padding-top: 80px;
+      // background-image: linear-gradient(to top, #9795f0 0%, #fbc8d4 100%);
+      &_right {
+        padding-top: 80px;
 
-      &_container {
-        width: 360px;
-        margin: 0 auto;
+        &_container {
+          width: 360px;
+          margin: 0 auto;
+        }
       }
     }
   }
-}
 
-.title_box {
-  display: flex;
+  .title_box {
+    display: flex;
 
-  .title {
-    font-size: 22px;
-    color: #000000;
+    .title {
+      font-size: 22px;
+      color: #000000;
+    }
   }
-}
 </style>
