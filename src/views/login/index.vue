64cf7968ff36a87ad9c6b9d04455d3de1118e0f7
zhenghy <1014660095@qq.com>
2022-06-08 16:54:53 +0800

    1

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,8 @@
     <a-card class="login" :bordered="false" :body-style="{ width: '480px', padding: '30px 0' }">
       <div class="login_right_container">
         <div class="title_box">
-          <h2 class="title">账密登录</h2>
+          <h2 class="title">
+            账密登录</h2>
         </div>
         <!-- form -->
         <login-form />
