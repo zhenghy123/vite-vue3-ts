3bec9348e941824da740e976bc4ab780f8f4451a
zhenghy <1014660095@qq.com>
2022-06-08 16:42:21 +0800

    1

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -12,32 +12,37 @@
   </div>
 </template>
 <script setup lang="ts">
-  import LoginForm from './Form.vue';
+import LoginForm from './Form.vue';
   // import { APP_TITLE } from '../../../config/constant';
 </script>
 <style lang="less" scoped>
-  .login-container {
-    width: 100vw;
-    height: 100vh;
-    background-image: linear-gradient(to right, #74ebd5 0%, #9face6 100%);
-    .login {
-      border-radius: 10px;
-      box-shadow: 0 3px 10px rgba(0, 0, 0, 0.4);
-      // background-image: linear-gradient(to top, #9795f0 0%, #fbc8d4 100%);
-      &_right {
-        padding-top: 80px;
-        &_container {
-          width: 360px;
-          margin: 0 auto;
-        }
+.login-container {
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(to right, #74ebd5 0%, #9face6 100%);
+
+  .login {
+    border-radius: 10px;
+    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.4);
+
+    // background-image: linear-gradient(to top, #9795f0 0%, #fbc8d4 100%);
+    &_right {
+      padding-top: 80px;
+
+      &_container {
+        width: 360px;
+        margin: 0 auto;
       }
     }
   }
-  .title_box {
-    display: flex;
-    .title {
-      font-size: 22px;
-      color: #000000;
-    }
+}
+
+.title_box {
+  display: flex;
+
+  .title {
+    font-size: 22px;
+    color: #000000;
   }
+}
 </style>
