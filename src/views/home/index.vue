78ba4bc3d74aa7c1557956c8502776dc1566ff44
zhenghy <1014660095@qq.com>
2022-06-09 13:25:43 +0800

    fix: ss
    xxx

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,27 +6,25 @@
   </div>
 </template>
 <script setup lang="ts">
-import { useHomeStore } from '/@/store/modules/home';
-import { storeToRefs } from 'pinia'
+  import { useHomeStore } from '/@/store/modules/home';
+  import { storeToRefs } from 'pinia';
 
-// pinia composition api
-const store = useHomeStore();
-const { count } = storeToRefs(store)
-// const count = computed(() => store.count)
-const increment = store.increment
+  // pinia composition api
+  const store = useHomeStore();
+  const { count } = storeToRefs(store);
+  // const count = computed(() => store.count)
+  const increment = store.increment;
 
-// pinia options api
-// const increment = () => {
-//   // store.increment()
-//   store.$patch({ count: store.count + 1 })
-// }
-
-
+  // pinia options api
+  // const increment = () => {
+  //   // store.increment()
+  //   store.$patch({ count: store.count + 1 })
+  // }
 </script>
 <style lang="less" scoped>
-.search {
-  & :deep(.ant-input) {
-    height: 48px;
+  .search {
+    & :deep(.ant-input) {
+      height: 48px;
+    }
   }
-}
 </style>
